c579d530317ec03bea25c9cb02d354217c9c6da3
keizo3 <keizo.suzuki3@gmail.com>
2018-09-27 16:16:14 +0900

    generate qr code css

--- src/actions/mfa/show.cr
+++ src/actions/mfa/show.cr
@@ -1,9 +1,15 @@
 class Mfa::Show < BrowserAction
   get "/mfa" do
-  	base32_secret = current_user.base32_secret.to_s
-  	diff = TOTP::DEFAULT_TIME_STEP_SECONDS - ((Time.now.epoch_ms / 1000) % TOTP::DEFAULT_TIME_STEP_SECONDS)
+    base32_secret = current_user.base32_secret.to_s
+    diff = TOTP::DEFAULT_TIME_STEP_SECONDS - ((Time.now.epoch_ms / 1000) % TOTP::DEFAULT_TIME_STEP_SECONDS)
     mfa_code = TOTP.generate_number_string(base32_secret)
 
-    render ShowPage , img_url: TOTP.qr_code_url(current_user.email, base32_secret), mfa_code: mfa_code
+    issuer = "test-mfa"
+    uri = URI.unescape(TOTP.otp_auth_url(issuer + ":" + current_user.email, base32_secret) + "%26issuer=" + issuer)
+    qrcode_data = QRencode::QRcode.new(uri)
+
+    google_qrcode_url = TOTP.qr_code_url(issuer + "-google:" + current_user.email, base32_secret) + "%26issuer=" + issuer + "-google"
+
+    render ShowPage, img_url: google_qrcode_url, mfa_code: mfa_code, qrcode_data: qrcode_data
   end
 end
--- src/pages/mfa/show_page.cr
+++ src/pages/mfa/show_page.cr
@@ -1,19 +1,35 @@
 class Mfa::ShowPage < MainLayout
   needs img_url : String
   needs mfa_code : String
+  needs qrcode_data : QRencode::QRcode
 
   def content
     h1 "MFA QR code"
+    text "Email:  #{@current_user.email}"
     helpful_tips
+    qr_code
+
+    text "now mfa code: #{@mfa_code}"
   end
 
   private def helpful_tips
-    h3 "Next, you may want to:"
+    h3 "Google API QRcode"
     ul do
-      li "Email:  #{@current_user.email}"
       li "imageURL:  #{@img_url}"
       img src: @img_url
-      li "now mfa code: #{@mfa_code}"
+    end
+  end
+
+  private def qr_code
+    h3 "CSS QRcode"
+    div class: "qrcode" do
+      @qrcode_data.each_row do |row|
+        div do
+          row.each do |byte|
+            span class: QRencode::Util.black?(byte) ? "b" : "w"
+          end
+        end
+      end
     end
   end
 end
